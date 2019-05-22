44bea314792d11262ad1eee816e535a257a08b80
Phurba Lama <phurbalama2002@gmail.com>
2019-05-22 15:53:11 -0400

    Update Final.java

--- Final.java
+++ Final.java
@@ -1,187 +1,225 @@
+package finalfinal;
+
 import java.util.Scanner;
 
-
-
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 
 public class Final {
-	
-	node head;
 
-	 class node{
-		String value;
-		node next;
+    node head;
+
+    class node {
+
+        String value;
+        node next;
+
+        public node() {
+            value = "";
+            next = null;
+        }
+
+        public node(String val) {
+            value = val;
+            next = null;
+        }
+    }
+
+    public void printlist() {
+        node tnode = head;
+
+        while (tnode != null) {
+            String[] name = tnode.value.split(" ");
+            String firstlast = name[1] + " " + name[0];
+            System.out.println(firstlast);
+
+            tnode = tnode.next;
+
+        }
+    }
+
+    public void saveList() throws IOException {
+        node tnode = head;
+        DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("share.dat"));
+
+        while (tnode != null) {
+            String[] name = tnode.value.split(" ");
+            String firstlast = name[1] + " " + name[0];
+
+            dataOut.writeBytes(firstlast + "\n");
+            tnode = tnode.next;
+
+        }
+        dataOut.close();
+    }
+
+    public void insert(String name) {
+try {
 		
-		public node() {
-			value = "";
-			next = null;
-		}
-	 
-		public node(String val)
-		{
-			value = val;
-			next = null;
-		}
-	 }
-	
-		public void printlist() {
-			node tnode = head;
-			
-			while(tnode!= null)
-			{
-				System.out.println(tnode.value);
-				tnode = tnode.next;
-				
-			}
-		}
-		public void push(String val) {
-			node newNode = new node(val);
-			newNode.next = head;
-			head = newNode;
-			}
-		 public static int getCountRec(node node) 
-		    { 
-		         
-		        if (node == null) 
-		            return 0; 
-		   
-		        return 1 + getCountRec(node.next); 
-		    } 
-		  
-		   
-		 public  int getCount() 
-		    { 
-		        return getCountRec(head); 
-		    }  
-		public void append(String new_data) 
+        node current = head;
+        node prev = new node();
+        prev.next = current;
+        
+        
+        String[] names = name.split(" ");
+        String lastfirst = names[1] + " " + names[0];
+        //System.out.println(lastfirst);
+        node newnode = new node(lastfirst);
+
+        if (head == null) {
+            head = new node(lastfirst);
+            return;
+        }
+
+        //for the head 
+        if (current.value.toLowerCase().compareTo(lastfirst.toLowerCase()) > 0) {
+
+            newnode.next = current;
+            this.head = newnode;
+            return;
+        }
+
+        while (current != null && current.value.toLowerCase().compareTo(lastfirst.toLowerCase()) <= 0) {
+        	
+
+            prev = current;
+            current = current.next;
+
+        }
+
+        newnode.next = current;
+        prev.next = newnode;
+//        if(getCount()>count) {
+//        	System.out.println(name+ " has been added");
+//        }
+//      
+}
+catch(Exception ex) {
+	System.out.println("Please Enter First and the Last Name correctly");
+}
+
+    }
+
+    public void delete(String name) {
+    	try {
+        node current = head;
+        node prev = new node();
+        prev.next = current;
+
+        String[] names = name.split(" ");
+        String lastfirst = names[1] + " " + names[0];
+
+        if (current.value.compareTo(lastfirst) == 0) {
+
+            head = current.next;
+            return;
+        }
+
+        while (current != null && current.value.compareTo(lastfirst) != 0) {
+            prev = current;
+            current = current.next;
+        }
+        prev.next = current.next;
+        System.out.println(name + " has been deleted");
+    	}
+    	catch(Exception ex) {
+    		System.out.println("Name doesnt exist");
+    	}
+    }
+	 public static int getCountRec(node node) 
 	    { 
-	       
-	        node new_node = new node(new_data); 
-	  
-	       
-	        if (head == null) 
-	        { 
-	            head = new node(new_data); 
-	            return; 
-	        } 
-	  
-	      
-	        new_node.next = null; 
-	  
-	    
-	        node last = head;  
-	        while (last.next != null) 
-	            last = last.next; 
-	  
-	       
-	        last.next = new_node; 
-	        return; 
+	         
+	        if (node == null) 
+	            return 0; 
+	   
+	        return 1 + getCountRec(node.next); 
 	    } 
-		public node bubbleSortList()
-	    {
-	        boolean isSwapped = true;
-	       
+	  
+	   
+	 public  int getCount() 
+	    { 
+	        return getCountRec(head); 
+	    }  
 
-	        for(node current = this.head, tail = null; isSwapped && head != tail; tail = current, current = head)
-	        {
-	            for(isSwapped = false; current.next != tail; current = current.next) 
-	            { 
-	                if (current.value.compareTo(current.next.value) >0) 
-	                {  
-	                    swap(current, current.next); 
-	                   
-	                    isSwapped = true; 
-	                }
-	            }
-	        }
-	        return head;
-	    }
+    public void load() throws IOException {
+        DataInputStream input = new DataInputStream(new FileInputStream("share.dat"));
 
-	    public static void swap(node x, node y)
-	    {
-	        if(x != y)
-	        {
-	            String temp = x.value;
-	            x.value = y.value;
-	            y.value = temp;    
-	        }
-	    }
-	    public void insert(String name)
-	    {
-	    	node current = head;
-	    	node prev = head;
-	    	
-	    	
-	    	while(current!=null && current.value.compareTo(name)<=0) {
-	    		prev = current;
-	    		current = current.next;
-	    		
-	    	}
-	    	node newnode = new node(name);
-	    	newnode.next = current;
-	    	prev.next = newnode;
-	    }
-	    public void delete(String name)
-	    {
-	    	node current = head;
-	    	node prev = head;
-	    	while(current!=null && current.value.compareTo(name)!=0)
-	    	{
-	    		prev = current;
-	    		current = current.next;
-	    	}
-	    	prev.next = current.next;
-	    	
-	    }
-	    
-	    
-		
-	
+        Final firstName = new Final();
+        int count = 0;
+        try {
+            while (input.available() > 0) {
+                String readName = input.readLine();
 
-	public static void main(String[] args) throws IOException{
-		
-		
-		DataInputStream input = new DataInputStream(new FileInputStream("share.dat"));
-		Final name = new Final();
-		Final firstName = new Final();
-		int count = 0;
-		while(input.available()>0) {
-			String readName = input.readLine();
-			
-			String [] fullName = readName.split(" ");
-			
-				
-				String lastandfirst = fullName[1]+" "+fullName[0]; //switches the last to first
-			//	System.out.println(lastandfirst);
-				
-				name.append(lastandfirst);
-				count++;
-			}
-//		System.out.println(count);
-//		System.out.println(firstName.getCount());
-//		System.out.println(name.getCount());
-		//name.printlist(); // prints the list
-			name.bubbleSortList(); //sorts the name linkedlist
-			
-			System.out.println("------------------");
-			System.out.println("After Sorting \n"+"------------------");
-			name.printlist();
-			name.insert("Apple John");
-			System.out.println("------------------");
-			System.out.println("After inserting \n"+"------------------");
-			name.printlist();
-			name.delete("Apple John");
-			System.out.println("------------------");
-			System.out.println("After deleting \n"+"------------------");
-			name.printlist();
-			
-			
-		}
-		
-	}
+                insert(readName);
 
+                count++;
+            }
+            System.out.println(count);
+        } catch (IOException ex) {
+            Logger.getLogger(Final.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
+    public static void main(String[] args) throws IOException {
 
+        do {
+            System.out.println("__________________Program Menu__________________________________________________By :Phurba Lama and Mohamed Ait iaza");
+            System.out.println("L: Load \t A: ADD \t D: Delete \tS: Save\t E: Exit");
+            
+            
 
+            String yesorno = "C";
 
+            Scanner s = new Scanner(System.in);
+            Final name = new Final();
+
+            yesorno = s.next();
+
+            switch (yesorno.toUpperCase()) {
+
+                case "L": {
+                    name.load();
+                    name.printlist();
+                    break;
+                }
+                case "A": {
+                    System.out.println("Enter the name you want to add");
+                    s.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+                    String newName = s.nextLine();
+                    name.load();
+                    name.insert(newName);
+                    name.saveList();
+                    
+                    break;
+                }
+                case "D": {
+                    System.out.println("Enter the name you want to delete");
+                    String delName = "";
+                    s.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+                    delName = s.nextLine();
+
+                    name.load();
+                    name.delete(delName);
+                    name.saveList();
+                   
+                    break;
+                }
+                case "E": {
+                    System.exit(0);
+                }
+                case "S": {
+                    name.load();
+                    name.saveList();
+                    System.out.println("Your files have been saved");
+                    break;
+                }
+            }//switch
+
+        } while (true);
+
+    }
+
+}
